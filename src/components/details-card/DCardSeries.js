7beb070c2dd716cd30b10c410962873fb359c2f9
HakamQadi <hakam.h.qadi@gmail.com>
2023-06-12 18:36:50 +0300

    done

--- src/components/details-card/DCardSeries.js
+++ src/components/details-card/DCardSeries.js
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const DCardSeries = () => {
-    const [movies, setMovies] = useState([]);
+    const [series, setSeries] = useState([]);
     const { id } = useParams();
     useEffect(() => {
 
         const fetchData = async () => {
             try {
                 const response = await Axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=acaf5616e65a3c1a99a3105366f786c6`);
-                const movieData = {
+                const seriesData = {
                     title: response.data.original_title,
                     rate: response.data.vote_average.toFixed(2),
                     description: response.data.overview,
@@ -18,7 +18,7 @@ const DCardSeries = () => {
                     bgPoster: `https://image.tmdb.org/t/p/original${response.data.backdrop_path}`,
                     poster: `https://image.tmdb.org/t/p/w300${response.data.poster_path}`
                 };
-                setMovies([movieData]);
+                setSeries([seriesData]);
             } catch (error) {
                 console.error(error);
             }
@@ -27,12 +27,12 @@ const DCardSeries = () => {
     },);
     return (
         <div className='poster-image'>
-            {movies.map((movie) => (
+            {series.map((serie) => (
                 <div
                     className="poster-image"
                     style={{
                         height: 'calc(100vh - 100px)',
-                        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${movie.bgPoster})`,
+                        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${serie.bgPoster})`,
                         backgroundRepeat: 'no-repeat',
                         backgroundPosition: 'center',
                         backgroundSize: 'cover',
@@ -40,7 +40,7 @@ const DCardSeries = () => {
                         placeItems: 'center',
 
                     }}
-                    key={movie.title}
+                    key={serie.title}
                 >
                     <div className='container' >
                         <div className="jumbotron" style={{
@@ -48,7 +48,7 @@ const DCardSeries = () => {
                             alignItems: 'center',
                         }}>
                             <div className='left_container p-4'>
-                                <h1 className="display-4 text-light fw-bolder">{movie.title}</h1>
+                                <h1 className="display-4 text-light fw-bolder">{serie.title}</h1>
                                 <div className="card-text"><div style={{
                                     padding: "1px 10px 1px 10px",
                                     marginBottom: '1rem',
@@ -58,15 +58,15 @@ const DCardSeries = () => {
                                     fontWeight: 'bolder',
                                     width: "fit-content"
 
-                                }}>{movie.rate}</div></div>
-                                <p className="lead text-light " >{movie.description}</p>
+                                }}>{serie.rate}</div></div>
+                                <p className="lead text-light " >{serie.description}</p>
                                 <div id='btn_container' >
                                     <button id='SecondBtn'>Trailer</button>
                                     <button id='PrimeBtn'>Watch Now</button>
                                 </div>
                             </div>
                             <div>
-                                <img id='details_img' src={movie.poster} alt="logo" style={{
+                                <img id='details_img' src={serie.poster} alt="logo" style={{
                                     height: '20rem',
                                     borderRadius: '17px'
                                 }} />
