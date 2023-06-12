22dbd58579eba21bdc3430028b4ff7e656fcd43c
HakamQadi <hakam.h.qadi@gmail.com>
2023-06-12 10:22:42 +0300

    done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-// import PrimeBtn from './../../components/button/PrimeBtn';
-// import SecondBtn from './../../components/button/SecondBtn';
+import PrimeBtn from './../../components/button/PrimeBtn';
+import SecondBtn from './../../components/button/SecondBtn';
 import Card from '../../components/main-card/Card';
 // import Nav from '../../components/navbar/Nav';
 // import Details from '../details/Details';
